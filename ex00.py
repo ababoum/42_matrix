4025addd4c5fa4098be45710af12cd5f458fcf9d
ababoum <ababou237@hotmail.com>
2023-05-27 18:05:27 +0200

    minor fix on ex00

--- ex00.py
+++ ex00.py
@@ -5,8 +5,8 @@ from matrix import Matrix
 
 
 if __name__ == "__main__":
-    u = Vector([2, 3])
-    v = Vector([5, 7])
+    u = Vector([2., 3.])
+    v = Vector([5., 7.])
 
     print(u + v)
     print(u - v)
@@ -15,8 +15,8 @@ if __name__ == "__main__":
 
     print('#' * 80)
 
-    u = Matrix([[1, 2], [3, 4]])
-    v = Matrix([[7, 4], [-2, 2]])
+    u = Matrix([[1., 2.], [3., 4.]])
+    v = Matrix([[7., 4.], [-2., 2.]])
 
     print(u + v)
     print(u - v)
